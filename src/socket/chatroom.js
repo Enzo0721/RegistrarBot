88911208e5392708c374c0b95f60fb6d30046781
noah <Noah>
2025-10-21 18:18:23 -0400

    setup the user verif so that there are 2 users in socket and for 1 user its llm

--- src/socket/chatroom.js
+++ src/socket/chatroom.js
@@ -1,26 +1,49 @@
 import config from "#config";
 
 export default (io) => {
-  io.on("connection", (socket) => {
-    config.log("A user connected:", socket.id);
+	const getCount = (roomId) =>
+		io.sockets.adapter.rooms.get(roomId)?.size ?? 0;
 
-    socket.on("join_room", (roomId) => {
-      socket.join(roomId);
-      config.log(`Socket ${socket.id} joined room ${roomId}`);
-      io.to(roomId).emit("user_joined", socket.id);
-    });
+	io.on("connection", (socket) => {
+		config.log("A user connected:", socket.id);
 
-    socket.on("chat_message", ({ roomId, message, user }) => {
-      io.to(roomId).emit("chat_message", {
-        user,
-        message,
-        timestamp: new Date(),
-      });
-    });
+		socket.on("join_room", (roomId) => {
+			const numUsers = getCount(roomId);
 
-    socket.on("disconnect", () => {
-      config.log("User disconnected:", socket.id);
-      io.emit("user_disconnected", socket.id);
-    });
-  });
+			if (numUsers >= 2) {
+				socket.emit("room_full", { roomId });
+				config.log(`Room ${roomId} is full. Socket ${socket.id} rejected.`);
+				return;
+			}
+
+			socket.join(roomId);
+			config.log(`Socket ${socket.id} joined room ${roomId}`);
+			socket.on("chat_message", ({ roomId, message, user }) => {
+				io.to(roomId).emit("chat_message", {
+					user,
+					message,
+					timestamp: new Date(),
+				});
+				if (getCount(roomId) == 1) {
+					io.to(roomId).emit("chat_message", {
+						user: 'system',
+						message: 'auto message, will be llm',
+						timestamp: new Date(),
+					});
+				}
+			});
+		});
+
+		socket.on("disconnecting", () => {
+			socket.rooms.forEach((roomId) => {
+				if (roomId !== socket.id) {
+					config.log(`Socket ${socket.id} left room ${roomId}`);
+				}
+			});
+		});
+
+		socket.on("disconnect", () => {
+			config.log("User disconnected:", socket.id);
+		});
+	});
 };
