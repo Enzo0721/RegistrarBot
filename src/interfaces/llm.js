b60810e8b4dff16927d64b644af880cf0040546c
lolnuggets <46546150+lolnuggets@users.noreply.github.com>
2025-11-14 17:17:21 -0500

    Update llm.js
    created the basic structure

--- src/interfaces/llm.js
+++ src/interfaces/llm.js
@@ -1,10 +1,34 @@
 import '#config';
 
-class LLMInterface {
-	
+const PROMPT = '';
 
-	constructor() {
+class LLM {
+	constructor(history = null) {
+		this.history = []; // prompt add herr
+		if (history == null) {
+			this.history = history;
+		}
+	}
+
+	async chat(message, role = 'user') {
+		const chat_history = history;
+		chat_history.push({
+			role,
+			message
+		});
+		const response = await fetch(config.llm, {
+			header: 'applicatiob/json',
+			body: JSON.stringify(chat_history)
+		}
+		if (!response.ok) {
+			config.error('LLM experienced fetch error');
+			throw new Error('LLM experienced fetch request error');
+		} else {
+			this.history = history;
+			const json = await response.json();
+			return json;
+		}
 	}
 }
 
-export { LLMInterface };
+export { LLM };
